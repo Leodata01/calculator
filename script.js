34deb1e5f7b599bcf63e11f9e31a07b3387072c7
leo <187560246+Leodata01@users.noreply.github.com>
2026-02-12 16:42:31 +0700

    add dom manipulation to react when buttons click

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // functions
 
-let oprator;
-let num1;
-let num2;
+let operator;
+let num1 = 0;
+let num2 = 0;
+let result = 0;
 
 function operate(num1, oprator, num2) {
-  let result = 0;
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
   switch (oprator) {
     case "+":
       result = add(num1, num2);
@@ -19,11 +21,11 @@ function operate(num1, oprator, num2) {
     case "/":
       result = divide(num1, num2);
       break;
-    case "/":
+    case "%":
       result = modulus(num1, num2);
       break;
   }
-  return result;
+  display.textContent = result;
 }
 
 function add(a, b) {
@@ -46,3 +48,39 @@ function modulus(a, b) {
   return a / b;
 }
 // Dom manipulation
+
+const numbersBtn = document.querySelectorAll(".number-btn");
+
+const display = document.querySelector("#displayText");
+
+// update 2 variable
+numbersBtn.forEach((number) => {
+  number.addEventListener("click", () => {
+    num2 = number.textContent;
+    display.textContent = num2;
+  });
+});
+
+// update operator variable
+
+operatorButtons = document.querySelectorAll(".operator-btn");
+
+operatorButtons.forEach((op) => {
+  op.addEventListener("click", () => {});
+});
+
+// return result when "=" pressed
+
+const equal = document.querySelector("#equal");
+
+equal.addEventListener("click", () => operate(num1, operator, num2));
+
+// Clear all when "Clear" pressed
+const clear = document.querySelector("#clear");
+
+clear.addEventListener("click", () => {
+  operator = "";
+  num1 = 0;
+  num2 = 0;
+  display.textContent = "";
+});
